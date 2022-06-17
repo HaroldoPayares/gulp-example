95ed52dfc553dda33081c8fec9328b8d108bae4b
Haroldo_Payares_Salgado <haroldo.payares@outlook.com>
2022-06-17 15:43:29 -0500

    minor changed on gulpfile.js file

--- gulpfile.js
+++ gulpfile.js
@@ -146,7 +146,7 @@ gulp.task('browser-sync', ['nodemon', 'scripts', 'styles'], function () {
         files: [CONFIG.SOURCE_ROOT+'/**/*.*'],
         proxy: 'http://localhost:3000',
         port: 4000,
-        browser: ['firefox.exe'],
+        open: false
     });
     gulp.watch(CONFIG.SOURCE_ROOT+'/**/*.{js,coffee}', ['scripts']);
     gulp.watch(CONFIG.SOURCE_ROOT+'/**/*.{css,less}',  ['styles']);
